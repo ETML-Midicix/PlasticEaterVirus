56489c47917fae8e3a0a687ccd7e921afdafb897
ETML-Midicix <119678382+ETML-Midicix@users.noreply.github.com>
2024-06-04 10:20:30 +0200

    Add files via upload

--- Program/script.py
+++ Program/script.py
@@ -1,2 +1,28 @@
-import os
-os.system("start powershell")
+from pynput.mouse import Button, Controller
+from time import sleep
+
+mouse = Controller()
+pos = (176, 115)
+posCoord = str(pos).replace('(', '').replace(')', '').replace(' ', '').split(',')
+posCoord[0] = int(posCoord[0])
+posCoord[1] = int(posCoord[1])
+
+#shift + F5 to stop
+while mouse.position != pos:
+    sleep(0.001)
+    tempPos = str(mouse.position).replace('(', '').replace(')', '').replace(' ', '').split(',')
+    tempPos[0] = int(tempPos[0])
+    tempPos[1] = int(tempPos[1])
+
+    x = 0
+    y = 0
+    if tempPos[0] > posCoord[0]:
+        x = -1
+    elif tempPos[0] < posCoord[0]:
+        x = 1
+    if tempPos[1] > posCoord[1]:
+        y = -1
+    elif tempPos[1] < posCoord[1]:
+        y = 1
+    
+    mouse.move(x ,y)
