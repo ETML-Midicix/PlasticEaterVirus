bf05f2792a404919fff41200d5784a7fd6e6c405
ETML-Midicix <pa64wln@eduvaud.ch>
2024-06-14 11:49:27 +0200

    Move Files

--- Program/background.bat
+++
@@ -1,2 +0,0 @@
-reg add "HKEY_CURRENT_USER\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d c:/plasticeater/plastic.bmp /f
-RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
--- Program/inkscape.bat
+++
@@ -1,4 +0,0 @@
-@ECHO OFF
-CLS
-curl https://inkscape.org/gallery/item/44619/inkscape-1.3.2_2023-11-25_091e20e-x64.msi -L -o inkscape.msi -s
-inkscape.msi
